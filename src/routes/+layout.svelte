d6405f0f4fd8e698c82dea9125f4f3e31af53d48
mohamedr <mramdane.dev@gmail.com>
2026-08-29 13:31:16 +0200

    fix: restaure Vercel et active Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,12 @@
 <script>
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 	import '../app.scss';
 	import 'iconify-icon/dist/iconify-icon.js';
 
 	import Snacks from '$lib/components/Snacks.svelte';
+
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 
 	let { children } = $props();
 </script>
